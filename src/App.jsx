1a785ba651ab7ef23b351d67f4f516211855e739
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2024-08-15 09:30:44 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-600">Vite + React</h1>
+      <button className="btn-ghost">Show me</button>
+    </>
+  )
+}
+
+export default App
