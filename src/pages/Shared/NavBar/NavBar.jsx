b7516b47268284fdc9c6dbd96228cfea8d9c766e
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2024-08-15 15:01:28 +0600

    Footer has been done

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,7 +1,18 @@
 import React from "react"
 import logo from "../../../assets/Logo/GadgetHub.png"
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
+  const navOptions = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>All Categories</a>
+      </li>
+    </>
+  )
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
@@ -26,52 +37,18 @@ const NavBar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {navOptions}
           </ul>
         </div>
         <img className="md:h-24 h-16 md:w-32 w-20" src={logo} alt="" />
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
+        <ul className="menu menu-horizontal px-1">{navOptions}</ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <Link to="/registration">
+          <li className="btn">Registration</li>
+        </Link>
       </div>
     </div>
   )
